2c431f8c8e8d0972719504e98a2aac22aa26e9ae
hayde <hayde0264@gmail.com>
2023-02-15 23:45:48 -0500

    autocomplete update

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -53,8 +53,13 @@ require('lspconfig')['cmake'].setup{
 
 
 -- AUTOCOMPLETION 
-local cmp = require'cmp'
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil 
+end 
 
+local cmp = require'cmp'
   cmp.setup({
     snippet = {
       -- REQUIRED - you must specify a snippet engine
@@ -69,7 +74,43 @@ local cmp = require'cmp'
       -- completion = cmp.config.window.bordered(),
       -- documentation = cmp.config.window.bordered(),
     },
+
     mapping = cmp.mapping.preset.insert({
+
+
+
+
+    ['<C-Space>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    },
+
+    ['<Tab>'] = function(fallback)
+      if not cmp.select_next_item() then
+        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end,
+
+    ['<S-Tab>'] = function(fallback)
+      if not cmp.select_prev_item() then
+        if vim.bo.buftype ~= 'prompt' and has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end
+    end,
+
+
+
+
+
+
+
       ['<C-b>'] = cmp.mapping.scroll_docs(-4),
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
