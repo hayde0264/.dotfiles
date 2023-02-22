2bdbc4a47b346ff041db2b51aadf3cfbe41720cc
Hayden H <hayde0264@gmail.com>
2023-02-21 21:02:05 -0500

    Update statusline.lua

--- .config/nvim/lua/statusline.lua
+++ .config/nvim/lua/statusline.lua
@@ -1,21 +1,13 @@
-local colors = {
-  blue   = '#80a0ff',
-  cyan   = '#99dac8',
-  black  = '#080808',
-  white  = '#c6c6c6',
-  red    = '#ff5189',
-  violet = '#d183e8',
-  grey   = '#303030',
-}
 
 local bubbles_theme = {
   normal = {
-    a = { fg = colors.black, bg = colors.violet },
-    b = { fg = colors.white, bg = colors.grey },
-    c = { fg = colors.black, bg = colors.black },
+    a = { fg = colors.black, bg = colors.green },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.red, bg = colors.black}, 
+    y = { fg = colors.cyan, bg = colors.black},
   },
 
-  insert = { a = { fg = colors.black, bg = colors.blue } },
+  insert = { a = { fg = colors.black, bg = colors.purple } },
   visual = { a = { fg = colors.black, bg = colors.cyan } },
   replace = { a = { fg = colors.black, bg = colors.red } },
 
@@ -36,10 +28,10 @@ require('lualine').setup {
     lualine_a = {
       { 'mode', separator = { left = '' }, right_padding = 2 },
     },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = { 'fileformat' },
+    lualine_b = { 'filename'},
+    lualine_c = {'branch'}, 
     lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
+    lualine_y = { 'progress' },
     lualine_z = {
       { 'location', separator = { right = '' }, left_padding = 2 },
     },
